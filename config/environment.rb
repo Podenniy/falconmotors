7201426940f66a05a7b6a3be85e384deb5fa9857
Dmitry <budyachocheck@gmail.com>
2013-12-06 18:19:10 +0200

    sendgerd4

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['Dima'],
-  :password       => ENV['123'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'falconmotors.herokuapp.com',
   :enable_starttls_auto => true
 }
